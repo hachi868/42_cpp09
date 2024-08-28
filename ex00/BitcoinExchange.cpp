53a2e7fe84430c41a9067713b99d87c64971b897
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-08-29 03:00:30 +0900

    [add]ex00 constructor etc

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,1 +1,38 @@
 #include "BitcoinExchange.hpp"
+
+const std::string BitcoinExchange::RESET = "\033[0m";
+const std::string BitcoinExchange::DEBUG = "\033[90m";
+const std::string BitcoinExchange::STATE = "\033[36m";
+const std::string BitcoinExchange::ALERT = "\033[31m";
+const std::string BitcoinExchange::MSG = "\033[34m";
+const std::string BitcoinExchange::ERROR = "\033[31m";
+
+BitcoinExchange::BitcoinExchange() : dataRate_() {
+    std::cout << BitcoinExchange::DEBUG << "[BitcoinExchange] constructor called" << BitcoinExchange::RESET << std::endl;
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &obj) : dataRate_(obj.dataRate_) {
+    std::cout << BitcoinExchange::DEBUG << "[BitcoinExchange] copy constructor called" << BitcoinExchange::RESET << std::endl;
+}
+
+BitcoinExchange &BitcoinExchange::operator = (const BitcoinExchange &obj) {
+    std::cout << BitcoinExchange::DEBUG << "[BitcoinExchange] assignation operator called" << BitcoinExchange::RESET << std::endl;
+    if (this != &obj) {
+        this->dataRate_ = obj.dataRate_;
+    }
+    return (*this);
+}
+
+BitcoinExchange::~BitcoinExchange() {
+    std::cout << BitcoinExchange::DEBUG << "[BitcoinExchange] destructor called" << BitcoinExchange::RESET << std::endl;
+}
+
+std::map<std::string, double> parseCsvToMap(const std::string& csvData) {
+    std::map<std::string, double> map;
+
+    return (map);
+}
+
+void putValue(std::string date) {
+
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -1,13 +1,31 @@
 #ifndef BITCOIN_EXCHANGE_HPP
 #define BITCOIN_EXCHANGE_HPP
 
-#include <algorithm>
+//#include <algorithm>
+//#include <stdio.h>
+//#include <iostream>
+#include <sstream>
+#include <map>
 
 class BitcoinExchange {
 public:
+    BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange &obj);
+    BitcoinExchange &operator = (const BitcoinExchange &obj);
+    ~BitcoinExchange();
+
+    std::map<std::string, double> parseCsvToMap(const std::string& csvData);
+    void putValue(std::string date);
+
+    const static std::string RESET;
+    const static std::string DEBUG;
+    const static std::string STATE;
+    const static std::string ALERT;
+    const static std::string MSG;
+    const static std::string ERROR;
 
 private:
-
+    std::map<std::string, double> dataRate_;
 };
 
 
